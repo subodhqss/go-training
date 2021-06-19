751bc4285db6d405ba09c2d3d5f3f789132c43f1
Alam <alamgeer.siddiqui@qsstechnosoft.com>
2021-06-19 23:14:08 +0530

    added employee files

--- main.go
+++ main.go
@@ -7,17 +7,40 @@ import (
 	"github.com/subodhqss/go-training/controllers"
 )
 
-func main() {
+func main(){
 	StartServer()
-	fmt.Println("Hello Trainee !")
 }
 
 func StartServer() {
 	r := controllers.NewRouter()
 	server := &http.Server{
-		Addr:    ":4000",
+		Addr:    ":5000",
 		Handler: r,
 	}
 	fmt.Println("Server is running at ", server.Addr)
 	server.ListenAndServe()
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"net/http"
+
+// 	"github.com/subodhqss/go-training/controllers"
+// )
+
+// func main() {
+// 	StartServer()
+// 	fmt.Println("Hello Trainee !")
+// }
+
+// func StartServer() {
+// 	r := controllers.NewRouter()
+// 	server := &http.Server{
+// 		Addr:    ":4000",
+// 		Handler: r,
+// 	}
+// 	fmt.Println("Server is running at ", server.Addr)
+// 	server.ListenAndServe()
+// }
