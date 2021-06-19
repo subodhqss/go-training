98b9bf55c25898f59961762551d9aecef350b4e4
Alam <alamgeer.siddiqui@qsstechnosoft.com>
2021-06-19 23:08:04 +0530

    added employee files

--- repository/employee_repository.go
+++ repository/employee_repository.go
@@ -17,11 +17,11 @@ type empRepo struct{}
 func (er *empRepo) PrintEmploye() *models.Employee {
 	emp := &models.Employee{
 		Name :            "ALAMGEER",
-		add :             "Meerut",
-		designation :     "Software trainee",
-		company_name :    "Qss Technosoft",
-		date_of_joining : "14-06-2021",
-		emp_code :        "522063",
+		Add :             "Meerut",
+		Designation :     "Software trainee",
+		Company_name :    "Qss Technosoft",
+		Date_of_joining : "14-06-2021",
+		EmpCode :        "522063",
 	}
 	return emp
 }
