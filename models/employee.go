dd1324aa1ab803931c2e0e8820750cefa311d553
Alam <alamgeer.siddiqui@qsstechnosoft.com>
2021-06-21 12:52:09 +0530

    edit is json

--- models/employee.go
+++ models/employee.go
@@ -5,6 +5,6 @@ type Employee struct {
 	Add             string `json:"address_of_employee"`
 	Designation     string `json:"Designation_of_Employee"`
 	Company_name    string `json:"Company_Name"`
-	JoiningDate 	string `json:"joining_date"`
+	Date_of_Joining string `json:"joining_date"`
 	EmpCode         string `json:"Employee_code"`
 }
--- repository/employee_repository.go
+++ repository/employee_repository.go
@@ -16,12 +16,12 @@ type empRepo struct{}
 
 func (er *empRepo) PrintEmploye() *models.Employee {
 	emp := &models.Employee{
-		Name :            "ALAMGEER",
-		Add :             "Meerut",
-		Designation :     "Software trainee",
-		Company_name :    "Qss Technosoft",
-		Date_of_joining : "14-06-2021",
-		EmpCode :        "522063",
+		Name:            "ALAMGEER",
+		Add:             "Meerut",
+		Designation:     "Software trainee",
+		Company_name:    "Qss Technosoft",
+		Date_of_Joining: "14-06-2021",
+		EmpCode:         "522063",
 	}
 	return emp
 }
