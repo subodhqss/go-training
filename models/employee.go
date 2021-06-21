14d7c96d743d4134085f1f5f80bc54c58aa3f53a
Alam <alamgeer.siddiqui@qsstechnosoft.com>
2021-06-21 14:08:26 +0530

    edit is json

--- models/employee.go
+++ models/employee.go
@@ -3,8 +3,8 @@ package models
 type Employee struct {
 	Name            string `json:"employee_name"`
 	Add             string `json:"address_of_employee"`
-	Designation     string `json:"Designation_of_Employee"`
-	Company_name    string `json:"Company_Name"`
+	Designation     string `json:"designation_of_Employee"`
+	Company_name    string `json:"company_name"`
 	Date_of_Joining string `json:"joining_date"`
-	EmpCode         string `json:"Employee_code"`
+	EmpCode         string `json:"employee_code"`
 }
