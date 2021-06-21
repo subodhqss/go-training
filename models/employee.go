412006e92513edf58f506c9ae539e589063b207b
Alam <alamgeer.siddiqui@qsstechnosoft.com>
2021-06-21 11:20:31 +0530

    change the json

--- models/employee.go
+++ models/employee.go
@@ -1,8 +1,8 @@
 package models
 
 type Employee struct {
-	Name            string `json:"Employee Name"`
-	Add             string `json:"Address Of Employee"`
+	Name            string `json:"employee_name"`
+	Add             string `json:"address_of_employee"`
 	Designation     string `json:"Designation of Employee"`
 	Company_name    string `json:"Company Name"`
 	Date_of_joining string `json:"joining date"`
