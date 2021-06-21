940ddc91312354b76a5a555ddbf797f41cd6bd39
gurneet <gurneet.kaur@qsstechnosoft.com>
2021-06-21 12:49:26 +0530

    done

--- models/Employee.go
+++ models/Employee.go
@@ -1,10 +1,10 @@
 package models
 
 type Employee struct {
-	Name            string `json:"Employee Name"`
-	Add             string `json:"Address Of Employee"`
-	Designation     string `json:"Designation of Employee"`
-	Company_name    string `json:"Company Name"`
-	Date_of_joining string `json:"joining date"`
-	EmpCode         string `json:"Employee code"`
+	Name            string `json:"Employee_Name"`
+	Add             string `json:"Address_Of_Employee"`
+	Designation     string `json:"Designation_of_Employee"`
+	Company_name    string `json:"Company_Name"`
+	Date_of_joining string `json:"joining_date"`
+	EmpCode         string `json:"Employee_code"`
 }
