1abe2765b5279513590566f6bdd131100929f2ff
Alam <alamgeer.siddiqui@qsstechnosoft.com>
2021-06-24 10:32:20 +0530

    1st entry showing

--- models/employee.go
+++ models/employee.go
@@ -1,10 +1,21 @@
 package models
 
 type Employee struct {
-	Name            string `json:"employee_name"`
-	Add             string `json:"address_of_employee"`
-	Designation     string `json:"designation_of_Employee"`
-	Company_name    string `json:"company_name"`
-	Date_of_Joining string `json:"joining_date"`
-	EmpCode         string `json:"employee_code"`
+	// gorm.Model
+	EmployeeNumber int    `gorm:"column:employeeNumber"json:"employee_number"`
+	LastName       string `gorm:"column:lastName"json:"last_name"`
+	FirstName      string `gorm:"column:firstName"json:"first_name"`
+	Extension      string `gorm:"column:extension"`
+	Email          string `gorm:"column:email"`
+	OfficeCode     string `gorm:"column:officeCode"json:"office_code"`
+	ReportTo       int    `gorm:"column:reportTo"json:"report_to"`
+	JobTitle       string `gorm:"column:jobTitle"json:"job_title"`
+
+	// Name            string `json:"employee_name"`
+	// Add             string `json:"address_of_employee"`
+	// Designation     string `json:"designation_of_Employee"`
+	// Company_name    string `json:"company_name"`
+	// Date_of_Joining string `json:"joining_date"`
+	// EmpCode         string `json:"employee_code"`
+
 }
--- repository/employee_repository.go
+++ repository/employee_repository.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"fmt"
+
 	"github.com/subodhqss/go-training/models"
 )
 
@@ -14,14 +16,38 @@ func NewEmpRepo() EmployeReposiotry {
 
 type empRepo struct{}
 
+// var employee models.Employee
+// var employees []models.Employee
+
 func (er *empRepo) PrintEmploye() *models.Employee {
-	emp := &models.Employee{
-		Name:            "ALAMGEER",
-		Add:             "Meerut",
-		Designation:     "Software trainee",
-		Company_name:    "Qss Technosoft",
-		Date_of_Joining: "14-06-2021",
-		EmpCode:         "522063",
-	}
-	return emp
+	// var employee models.Employee
+	// var employees [] empRepo
+
+	// result := map[string]interface{}{}
+	// gormDB.Table("employees").Take(&result)
+
+	// var Employee [] models.Employee
+
+	// db.Find(&employee)
+
+	e := &models.Employee{}
+
+	res := gormDB.Limit(10).Take(e)
+
+	fmt.Println("res ", res.RowsAffected)
+
+	// gormDB.Raw("select * from employees limit 10").Scan(&e)
+
+	// gormDB.Select("Empnumber", "LastName", "FirstName",
+	// "Extension", "Email", "OfficeCode", "ReportTo", "JobTitle").Create(&er)
+
+	// emp := &models.Employee{
+	// 	Name:            "ALAMGEER",
+	// 	Add:             "Meerut",
+	// 	Designation:     "Software trainee",
+	// 	Company_name:    "Qss Technosoft",
+	// 	Date_of_Joining: "14-06-2021",
+	// 	EmpCode:         "522063",
+	// }
+	return e
 }
