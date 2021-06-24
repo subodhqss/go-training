10769c0c8f174c29026fbc7cfa0a6b1d61e2da72
Alam <alamgeer.siddiqui@qsstechnosoft.com>
2021-06-24 12:09:39 +0530

    10 users data

--- repository/database.go
+++ repository/database.go
@@ -12,7 +12,7 @@ var gormDB *gorm.DB
 
 func DBInit() {
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
-	dsn := "root:Alam@official@/classicmodels"
+	dsn := "root:8520@/classicmodels"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Unable to connect to Gorm, exiting: %v", err)
