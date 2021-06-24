a6158e2c759b670a2edbe68e75bd0a90f5f2aff2
Alam <alamgeer.siddiqui@qsstechnosoft.com>
2021-06-24 11:51:53 +0530

    get data

--- models/employee.go
+++ models/employee.go
@@ -1,7 +1,7 @@
 package models
 
 type Employee struct {
-	// gorm.Model
+	
 	EmployeeNumber int    `gorm:"column:employeeNumber"json:"employee_number"`
 	LastName       string `gorm:"column:lastName"json:"last_name"`
 	FirstName      string `gorm:"column:firstName"json:"first_name"`
@@ -11,11 +11,6 @@ type Employee struct {
 	ReportTo       int    `gorm:"column:reportTo"json:"report_to"`
 	JobTitle       string `gorm:"column:jobTitle"json:"job_title"`
 
-	// Name            string `json:"employee_name"`
-	// Add             string `json:"address_of_employee"`
-	// Designation     string `json:"designation_of_Employee"`
-	// Company_name    string `json:"company_name"`
-	// Date_of_Joining string `json:"joining_date"`
-	// EmpCode         string `json:"employee_code"`
+	
 
 }
--- repository/employee_repository.go
+++ repository/employee_repository.go
@@ -1,53 +1,32 @@
 package repository
 
 import (
-	"fmt"
+	"log"
 
 	"github.com/subodhqss/go-training/models"
 )
 
 type EmployeReposiotry interface {
-	PrintEmploye() *models.Employee
+	PrintEmploye() []*models.Employee
 }
 
 func NewEmpRepo() EmployeReposiotry {
 	return &empRepo{}
+	
 }
 
 type empRepo struct{}
 
-// var employee models.Employee
-// var employees []models.Employee
 
-func (er *empRepo) PrintEmploye() *models.Employee {
-	// var employee models.Employee
-	// var employees [] empRepo
 
-	// result := map[string]interface{}{}
-	// gormDB.Table("employees").Take(&result)
+func (er *empRepo) PrintEmploye()[] *models.Employee {
+	
 
-	// var Employee [] models.Employee
+	var employee []*models.Employee
+	result := gormDB.Limit(10).Find(&employee)
+	if err := result.Error; err != nil {
+		log.Print("Error in getting all records")
+	}
 
-	// db.Find(&employee)
-
-	e := &models.Employee{}
-
-	res := gormDB.Limit(10).Take(e)
-
-	fmt.Println("res ", res.RowsAffected)
-
-	// gormDB.Raw("select * from employees limit 10").Scan(&e)
-
-	// gormDB.Select("Empnumber", "LastName", "FirstName",
-	// "Extension", "Email", "OfficeCode", "ReportTo", "JobTitle").Create(&er)
-
-	// emp := &models.Employee{
-	// 	Name:            "ALAMGEER",
-	// 	Add:             "Meerut",
-	// 	Designation:     "Software trainee",
-	// 	Company_name:    "Qss Technosoft",
-	// 	Date_of_Joining: "14-06-2021",
-	// 	EmpCode:         "522063",
-	// }
-	return e
+		return employee
 }
