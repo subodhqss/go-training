64e1a3d7843f2a6618c81b234492ed72a215899c
Alam <alamgeer.siddiqui@qsstechnosoft.com>
2021-06-30 12:40:58 +0530

    database.go new file added

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	repository.DBInit()
+	repository.InitDBConnection()
 	StartServer()
 	
 
@@ -25,26 +25,3 @@ func StartServer() {
 	server.ListenAndServe()
 }
 
-// package main
-
-// import (
-// 	"fmt"
-// 	"net/http"
-
-// 	"github.com/subodhqss/go-training/controllers"
-// )
-
-// func main() {
-// 	StartServer()
-// 	fmt.Println("Hello Trainee !")
-// }
-
-// func StartServer() {
-// 	r := controllers.NewRouter()
-// 	server := &http.Server{
-// 		Addr:    ":4000",
-// 		Handler: r,
-// 	}
-// 	fmt.Println("Server is running at ", server.Addr)
-// 	server.ListenAndServe()
-// }
