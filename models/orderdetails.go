16b60176893542efe458ab9864fbe1046f68e562
Alam <alamgeer.siddiqui@qsstechnosoft.com>
2021-07-12 13:28:26 +0530

    Relations between oders and orderdetails table

--- models/orderdetails.go
+++ models/orderdetails.go
@@ -2,7 +2,7 @@ package models
 
 type Orderdetail struct {
 	OrderNumber     int     `gorm:"column:orderNumber" json:"OrderNumber"`
-	ProductCode     string  `gorm:"column:productCode " json:"productCode "`
+	ProductCode     string `gorm:"column:productCode" json:"product_code"`
 	QuantityOrdered int     `gorm:"column:quantityOrdered" json:"quantityOrdered"`
 	PriceEach       float64 `gorm:"column:priceEach" json:"priceEach"`
 	OrderLineNumber int     `gorm:"column:orderLineNumber" json:"orderLine_Number"`
