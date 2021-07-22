670ab3702f9c5bdf7dfc6a0c6900cd9e7412f2be
gurneet <gurneet.kaur@qsstechnosoft.com>
2021-07-22 17:08:06 +0530

    login

--- controllers/handler_controller.go
+++ controllers/handler_controller.go
@@ -1,83 +1,123 @@
-package controllers
+ package controllers
+
+
 
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"time"
+
 	"github.com/dgrijalva/jwt-go"
 )
 
-//var jwtKey = []byte("secret_key")
-/*var employee = map[string]string{
-	"gbondur@classicmodelcars.com": "PSWD9",
-	"dmurphy@classicmodelcars.com":"PSWD4",
-}*/
+var jwtKey = []byte("secret_key")
+
+var users = map[string]string{
+	"user2": "password2",
+
+}
+
+type Credentials struct {
+	Email string `json:"email"`
+	Password string `json:"password"`
+}
+
+type Claims struct {
+	Email string `json:"email"`
+	jwt.StandardClaims
+}
+
+type ResponseData struct {
+	Expires time.Time `json:"expire"`
+	Token   string    `json:"token"`
+}
 
 func Login(w http.ResponseWriter, r *http.Request) {
+
 	var credentials Credentials
 	err := json.NewDecoder(r.Body).Decode(&credentials)
 	if err != nil {
+		log.Print("Err1 : ", err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	
-	}
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	tokenString, err := token.SignedString(jwtKey)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	http.SetCookie(w,
-		&http.Cookie{
-			Name:    "token",
-			Value:   "tokenString",
-			Expires: "expirationTime",
-		})
+	expectedPassword, ok := users[credentials.Email]
 
-}
-func Home(w http.ResponseWriter, r *http.Request) {
-	cookie, err := r.Cookie("token"
-	if err != nil {
-		if err == http.ErrNoCookie {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		tokenStr := cookie.Value
-		claims := &Claims{}
-		tkn, err := jwt.ParseWithClaims(tokenStr, claims,
-			func(t *jwt.Token) (interface{}, error) {
-				return jwtKey, nil
-				expectedPassword, ok := employee[credentials.Email]
 	if !ok || expectedPassword != credentials.Password {
 		w.WriteHeader(http.StatusUnauthorized)
+		log.Print("Err2 : ", err)
 		return
 	}
+
 	expirationTime := time.Now().Add(time.Minute * 5)
 	claims := &Claims{
 		Email: credentials.Email,
 		StandardClaims: jwt.StandardClaims{
 			ExpiresAt: expirationTime.Unix(),
 		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Print("Err3 : ", err)
+		return
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:    "token",
+		Value:   tokenString,
+		Expires: expirationTime,
+	})
+
+	// res := &ResponseData{
+	// 	Token:   tokenString,
+	// 	Expires: expirationTime,
+	// }
+
+	jsonData, _ := json.Marshal(claims)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write(jsonData)
+
 }
 
-			})
-		if err != nil {
-			if err == jwt.ErrSignatureInvalid {
-				w.WriteHeader(http.StatusUnauthorized)
-				return
-			}
-		 	w.WriteHeader(http.StatusBadRequest)
-			return    
-
-		}
-		if !tkn.Valid {
+func Home(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("token")
+	if err != nil {
+		if err == http.ErrNoCookie {
 			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
-		w.Write([]byte(fmt.Sprintf("Hello, %s", claims.Email)))
-		
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	tokenStr := cookie.Value
+
+	claims := &Claims{}
+
+	tkn, err := jwt.ParseWithClaims(tokenStr, claims,
+		func(t *jwt.Token) (interface{}, error) {
+			return jwtKey, nil
+
+		})
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
 
+	if !tkn.Valid {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	w.Write([]byte(fmt.Sprintf("Hello, %s", claims.Email)))
+
 }
+
--- go.mod
+++ go.mod
@@ -3,8 +3,7 @@ module github.com/subodhqss/go-training
 go 1.16
 
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
-	github.com/dvsekhvalnov/jose2go v1.5.0 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible 
 	github.com/gorilla/mux v1.8.0
 	gorm.io/driver/mysql v1.1.1
 	gorm.io/gorm v1.21.11
