57c79dbbf67a3add5812ce80ab63cd25d15cfc44
gurneet <gurneet.kaur@qsstechnosoft.com>
2021-08-02 14:34:37 +0530

    pswd

--- controllers/employee_controller.go
+++ controllers/employee_controller.go
@@ -1,19 +1,20 @@
 package controllers
 
 import (
-	"log"
 	"encoding/json"
+	"log"
 	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/subodhqss/go-training/models"
 	"github.com/subodhqss/go-training/repository"
-	"github.com/subodhqss/go-training/services"
+	service "github.com/subodhqss/go-training/services"
 	"golang.org/x/crypto/bcrypt"
 )
 
-
 var employeService = service.NewEmployeService(repository.NewEmpRepo())
-//Employee model functions 
+
+//Employee model functions
 func GetEmploye(rw http.ResponseWriter, r *http.Request) {
 	data := employeService.PrintEmploye()
 	jsonData, _ := json.Marshal(data)
@@ -22,7 +23,7 @@ func GetEmploye(rw http.ResponseWriter, r *http.Request) {
 	rw.Write(jsonData)
 }
 func GetEmployeId(rw http.ResponseWriter, r *http.Request) {
-	
+
 	vars := mux.Vars(r)["eid"]
 	data := employeService.PrintEmployeId(vars)
 	jsonData, _ := json.Marshal(data)
@@ -32,7 +33,7 @@ func GetEmployeId(rw http.ResponseWriter, r *http.Request) {
 }
 
 /*func GetEmployeEmail(rw http.ResponseWriter, r *http.Request) {
-	
+
 	vars := mux.Vars(r)["email"]
 	data := employeService.PrintEmployeEmail(vars)
 	jsonData, _ := json.Marshal(data)
@@ -43,20 +44,20 @@ func GetEmployeId(rw http.ResponseWriter, r *http.Request) {
 func CreateEmployee(rw http.ResponseWriter, r *http.Request) {
 	rw.Header().Set("Content-Type", "application/json")
 	var Employee models.Employee
-	var emp *models.Employee
-	var login *models.Login
-	var err error 
 	json.NewDecoder(r.Body).Decode(&Employee)
-	employeService.SaveEmployee(Employee)
-	json.NewEncoder(rw).Encode("Employee is Created Succesfully !")
-	json.NewEncoder(rw).Encode(Employee)
-	hasPass,err := bcrypt.GenerateFromPassword([]byte(login.Password),5)
-	log.Print("hased Pass :", string(hasPass), emp.Password)
+
+	hasPass, err := bcrypt.GenerateFromPassword([]byte(Employee.Password), 5)
+	log.Print("hased Pass :", string(hasPass), Employee.Password)
 	if err != nil {
-		return 
+		return
 	}
 
-	log.Print("hased Pass :", string(hasPass), emp.Password)
+	Employee.Password = string(hasPass)
+
+	data := employeService.SaveEmployee(Employee)
+	json.NewEncoder(rw).Encode(data)
+
+	// log.Print("hased Pass :", string(hasPass), emp.Password)
 
 }
 
@@ -82,6 +83,6 @@ func DeleteEmployee(rw http.ResponseWriter, r *http.Request) {
 	rw.Header().Set("Content-Type", "application/json")
 	var Employee models.Employee
 	vars := mux.Vars(r)["eid"]
-	employeService.DeleteEmployee(Employee,vars)
-	json.NewEncoder(rw).Encode("Employee number: "+vars+" Deleted Succesfully !")
+	employeService.DeleteEmployee(Employee, vars)
+	json.NewEncoder(rw).Encode("Employee number: " + vars + " Deleted Succesfully !")
 }
--- services/auth_service.go
+++ services/auth_service.go
@@ -2,7 +2,7 @@ package service
 
 import (
 	"log"
-
+	"fmt"
 	"github.com/subodhqss/go-training/models"
 	"github.com/subodhqss/go-training/repository"
 	"golang.org/x/crypto/bcrypt"
@@ -25,7 +25,14 @@ func (ls *logServ) LoginEmployee(login *models.Login) *models.Login {
 		return nil
 	}
 	// compare the password here using bcrypt
-	hasPass, err := bcrypt.GenerateFromPassword([]byte(login.Password), 5)
+	//password := "pswd1"
+	err := bcrypt.CompareHashAndPassword([]byte(emp.Password), []byte(login.Password))
+	if err != nil {
+		log.Printf("Error: comparing pass %s and %s", emp.Password, login.Password)
+	} else {
+		fmt.Println("Pass match")
+	}
+	/*hasPass, err := bcrypt.GenerateFromPassword([]byte(login.Password), 5)
 	log.Print("hased Pass :", string(hasPass), emp.Password)
 	if err != nil {
 		return nil
@@ -35,11 +42,13 @@ func (ls *logServ) LoginEmployee(login *models.Login) *models.Login {
 
 	if bcrypt.CompareHashAndPassword([]byte(emp.Password), hasPass) != nil {
 		return nil
-	}
+	}I*/
 	loginData := &models.Login{
 		Email:     emp.Email,
 		FirstName: emp.FirstName,
 		LastName:  emp.LastName,
+		EmployeeNumber: emp.EmployeeNumber,
+		
 	}
 	return loginData
 }
