080ea81f44159aa2c3f41013df809fb337d313b6
alam <alamgeer.siddiqui@qsstechnosoft.com>
2021-08-10 10:22:51 +0530

    save employee test case

--- repository/employee_repository_test.go
+++ repository/employee_repository_test.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	// "log"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,19 +10,22 @@ import (
 
 func TestSaveEmployee1(t *testing.T){
 	repo := NewEmpRepo()
-	expRes := &models.Employee{
-		EmployeeNumber: 112,
-		LastName:       "aasda",
-		FirstName:      "dada",
-		Extension:      "adsdaassd",
-		Email:          "faf@sd.com",
-		// OfficeCode   : 113 ,
-		// OfficeDetail: "fgdg",
-		// // ReportsTo  : "dfgd",
-		ReportsToId: 113,
-		JobTitle:    "hfhfhf",
+	employeeDummy := &models.Employee{
+		EmployeeNumber: 111,
+		LastName:       "Diane",
+		FirstName:      "Murphy",
+		Extension:      "x5800",
+		Email:          "dmurphy@classicmodelcars.com",
+		OfficeCode   : "1" ,
+		ReportsToId: 1002,
+		JobTitle:    "President",
 	}
 
-	res := repo.SaveEmployee1(models.Employee{})
-	assert.Equal(t,expRes,res)
+
+	res := repo.SaveEmployee(*employeeDummy)
+	assert.Equal(t,res,employeeDummy)
+
+	errCheck := repo.SaveEmployee(models.Employee{})
+	assert.NotNil(t,errCheck)
+
 }
